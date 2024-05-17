c4ea4dfb19a1591d595fbe500bb414fcb35cf3d0
oscar <oscar.vasquez@zentro.com.mx>
2024-05-17 17:39:54 -0600

    debug

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const useCount = () => {
+
+  const [counter, setCounter] = useState(0);
+  const increase = () => setCounter(counter + 1);
+  const decrement = () => setCounter(counter - 1);
+  const reset = () => setCounter(0);
+
+  return {
+    counter,
+    increase,
+    decrement,
+    reset
+  }
+}
 
 function App() {
+
+  CONST {counter , increase, decrement, reset} = useCount();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <div>{counter}</div>
+    <button onClick={increase}>+</button>
+    <button onClick={decrement}>-</button>
+    <button onClick={reset}>Zero</button>
+   </div>
   );
 }
 
